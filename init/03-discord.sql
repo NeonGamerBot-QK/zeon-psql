67ea492e990d329da827b6cf77ce015106369e58
Saahil <neon@saahild.com>
2025-09-07 00:24:37 -0400

    update: irl updates table

---
+++ init/03-discord.sql
@@ -0,0 +1,16 @@
+-- sql sql sql
+CREATE TABLE IF NOT EXISTS irl_updates (
+    id SERIAL PRIMARY KEY,
+    lat DOUBLE PRECISION NOT NULL,
+    long DOUBLE PRECISION NOT NULL,
+    addr TEXT NOT NULL,
+    city TEXT NOT NULL,
+    name TEXT NOT NULL,
+    clipboard TEXT NOT NULL,
+    focus TEXT NOT NULL,
+    battery INT NOT NULL,
+    weather TEXT NOT NULL,
+    created_at BIGINT NOT NULL,
+    type TEXT
+);
+
